1770c0593dbb04b1bb61f0bcf181735c22928718
sachin449 <sachinpanghal2001@gmail.com>
2024-06-26 22:45:45 +0530

    firstcommit

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Mossam from './Components/Mossam'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    < Mossam />
+    </>
+  )
+}
+
+export default App
---
+++ weather-app/src/Components/Mossam.jsx
@@ -0,0 +1,11 @@
+
+
+
+const Mossam = () => {
+    return(
+       <div className="bg-black min-h-screen w-screen h-screen  ">
+       </div>
+    );
+};
+
+export default Mossam;
